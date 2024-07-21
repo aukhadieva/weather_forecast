bc4a61fc5613979f667924d7bf05d363b5d6e64e
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-21 16:13:02 +0500

    Include weather.urls

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
+    path('', include('weather.urls', namespace='weather')),
 ]
