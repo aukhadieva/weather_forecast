5be7c3d937bbf917ea0d29f96a5be7296f1aaaa8
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-21 08:44:01 +0500

    Add UserLoginView + UserRegisterView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from common.mixins import TitleMixin
+from users.forms import UserLoginForm, UserRegistrationForm
+from users.models import User
+
+
+class UserLoginView(TitleMixin, LoginView):
+    """
+    Класс-представление для страницы авторизации пользователя.
+    """
+    model = User
+    form_class = UserLoginForm
+    template_name = 'users/login.html'
+    title = 'Авторизация'
+
+
+class UserRegisterView(TitleMixin, CreateView):
+    """
+    Класс-представление для страницы регистрации пользователя.
+    """
+    model = User
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy('users:login')
+    template_name = 'users/register.html'
+    title = 'Регистрация'
+
+    def form_valid(self, form):
+        """
+        Сохраняет созданного пользователя, а также активирует его.
+        """
+        user = form.save()
+        user.is_active = True
+        user.save()
+        return super().form_valid(form)
