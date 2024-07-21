c5cda2d9c31ea01e94ba093a5533b30d21a41135
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-21 18:43:09 +0500

    Add get_locations_count method (WeatherSerializer)

--- weather/serializers.py
+++ weather/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from rest_framework import serializers
 
 from weather.models import Weather
@@ -7,7 +8,14 @@ class WeatherSerializer(serializers.ModelSerializer):
     """
     Сериализатор для модели Habits
     """
+    locations_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Weather
         fields = '__all__'
+
+    def get_locations_count(self, obj):
+        """
+        Возвращает количество городов.
+        """
+        return Weather.objects.filter(location=obj.location).count()
